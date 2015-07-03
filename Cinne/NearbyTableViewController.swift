7f89992ba2ac9c25d0b6d36900f7066fa70413a6
Gustavo Barbosa <gustavocsb@gmail.com>
2015-07-03 16:59:13 -0400

    Initial commit

---
+++ Cinne/NearbyTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NearbyTableViewController.swift
+//  Cinne
+//
+//  Created by Gustavo Barbosa on 6/25/15.
+//  Copyright (c) 2015 7pixels. All rights reserved.
+//
+
+import UIKit
+
+class NearbyTableViewController: UITableViewController {
+
+}
